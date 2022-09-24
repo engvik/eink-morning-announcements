0fe628a0348d78cac5960c722b611c79ccdbbce2
Lars Jansøn Engvik <lars@engvik.nu>
2022-09-24 22:51:00 +0200

    feature: hello world

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+#include <GxEPD2_BW.h>
+
+GxEPD2_BW<GxEPD2_750_T7, GxEPD2_750_T7::HEIGHT/2> display(GxEPD2_750_T7(/*CS=*/15, /*DC=*/ 27, /*RST=*/ 26, /*BUSY=*/ 25));
 
 void setup() {
-  // put your setup code here, to run once:
+  display.init();
+  display.setTextColor(GxEPD_BLACK);
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.print("Hello boiiiiii");
+  }
+  while (display.nextPage());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() {}
