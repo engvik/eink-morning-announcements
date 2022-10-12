63108179232c8c0a349b178ba860d2274b534ab0
Lars Jansøn Engvik <lars@engvik.nu>
2022-10-12 19:13:29 +0200

    fix: working hello world

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 #include <GxEPD2_BW.h>
 
-GxEPD2_BW<GxEPD2_750_T7, GxEPD2_750_T7::HEIGHT/2> display(GxEPD2_750_T7(/*CS=*/15, /*DC=*/ 27, /*RST=*/ 26, /*BUSY=*/ 25));
+GxEPD2_BW<GxEPD2_750_T7, GxEPD2_750_T7::HEIGHT> display(GxEPD2_750_T7(/*CS=*/5, /*DC=*/ 0, /*RST=*/ 2, /*BUSY=*/ 15));
 
-void setup() {
-  display.init();
+void setup()
+{
+
+  display.init(115200);
+
   display.setTextColor(GxEPD_BLACK);
   display.firstPage();
   do
   {
     display.fillScreen(GxEPD_WHITE);
-    display.print("Hello boiiiiii");
+    display.print("Hello World!");
   }
   while (display.nextPage());
+
+  display.hibernate();
 }
 
-void loop() {}
+void loop() {};
